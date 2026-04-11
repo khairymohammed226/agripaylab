7b11a668050b685b19c90b538cb20bacc3d2a2f9
Youssef <youssefhamada171y@gmail.com>
2026-04-11 14:36:54 +0200

    show full mongo error

--- server.js
+++ server.js
@@ -46,16 +46,17 @@ const limiter = rateLimit({
 mongoose.set("strictQuery", true);
 console.log("ENV TEST:", process.env.MONGO);
 
-mongoose.connect(process.env.MONGO)
-  .then(() => {
-    console.log("MongoDB Connected ✅");
-
-    console.log("DB NAME:", mongoose.connection.name);
-    console.log("DB HOST:", mongoose.connection.host);
-  })
-  .catch((err) => {
-    console.error("MongoDB Error ❌:", err);
-  });    
+mongoose.connect(process.env.MONGO, {
+  dbName: "onlineBankingDB"
+})
+.then(() => {
+  console.log("MongoDB Connected ✅");
+  console.log("DB NAME:", mongoose.connection.name);
+  console.log("DB HOST:", mongoose.connection.host);
+})
+.catch((err) => {
+  console.error("🔥 MongoDB FULL ERROR:", err);
+});   
     // Middlewares
   
     
